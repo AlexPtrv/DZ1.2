c32072d00e3f4f1ed1e55190c0ffa18b4ec5f705
Alex Petrov <2581653@ukr.net>
2017-04-14 19:46:29 +0300

    Add class

--- src/Go.java
+++ src/Go.java
@@ -1,13 +1,12 @@
 import java.util.*;
 public class Go {
     public static void main(String[] args) {
-Scanner name=new Scanner(System.in);
+        Scanner name=new Scanner(System.in);
         System.out.println("Enter your name:");
         String Name=name.next();
         System.out.println("Hello, "+Name);
+    }}
 
 
 
 
-    }
-}
