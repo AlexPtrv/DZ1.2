cf01aca34e6e41e12ed39f8f8142bd8c4febcc59
Alex Petrov <2581653@ukr.net>
2017-04-14 22:22:24 +0300

    Add class dz2

---
+++ src/dzsecond.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class dzsecond{
+    public static void main(String[]args){
+    Scanner number=new Scanner(System.in);
+    int[] mas=new int[6];
+System.out.println("Enter number 1:");
+        mas[1]=number.nextInt();
+        System.out.println("Enter number 2:");
+        mas[2]=number.nextInt();
+        System.out.println("Enter number 3:");
+        mas[3] =number.nextInt();
+        System.out.println("Enter number 4:");
+        mas [4]=number.nextInt();
+        System.out.println("Enter number 5:");
+        mas[5]=number.nextInt();
+        System.out.println("Initial array:"+mas[1]+","+mas[2]+","+mas[3]+","+mas[4]+","+mas[5]);
+        System.out.print("Sorted array:");
+        Arrays.sort(mas);
+        for(int i = 1; i < mas.length; i++)
+        {
+
+        System.out.print(mas[i]+",");
+        }
+
+        }}
